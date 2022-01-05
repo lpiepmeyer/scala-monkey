9aade3d690fac84e3330157b83019f32601a6017
lothar <lotharp@ixteam.de>
2022-01-05 16:31:00 +0100

    removed toString methods from AST classes

--- src/main/scala/de/hfu/parser/AST.scala
+++ src/main/scala/de/hfu/parser/AST.scala
@@ -2,37 +2,34 @@ package de.hfu.parser
 
 import de.hfu.lexer.Token
 
-abstract class Node {
+abstract class Node {}
 
-}
 abstract class Statement() extends Node
+
 abstract class Expression() extends Node
+
 case class Program(statements: List[Statement])
+
 case class BlockStatement(statements: List[Statement])
 
-case class  IfExpression(condition : Expression, consequence:BlockStatement, alternative:Option[BlockStatement]) extends Expression
+case class IfExpression(condition: Expression, consequence: BlockStatement, alternative: Option[BlockStatement]) extends Expression
 
+case class LetStatement(name: String, expression: Expression) extends Statement
 
-case class LetStatement(name :String, expression: Expression) extends Statement{
-  override def toString: String ="let "+name+" = "+expression
-}
-case class ReturnStatement(expression: Expression) extends Statement{
-  override def toString()="return "+expression
-}
+case class ReturnStatement(expression: Expression) extends Statement
+
 case class ExpressionStatement(expression: Expression) extends Statement
 
-case class BoolLiteral(value: Boolean) extends Expression{
-  override def toString()=value.toString
-}
-case class IntegerLiteral(value: Int) extends Expression{
-  override def toString()=value.toString
-}
-case class Identifier(value: String) extends Expression{
-  override def toString()=value.toString
-}
-case class PrefixExpression(operator: Token, right: Expression) extends Expression
-case class InfixExpression(operator: Token,left: Expression, right: Expression) extends Expression
+case class BoolLiteral(value: Boolean) extends Expression
 
-case class  FunctionLiteral(parameters: List[Identifier], body: BlockStatement) extends Expression
+case class IntegerLiteral(value: Int) extends Expression
+
+case class Identifier(value: String) extends Expression
+
+case class PrefixExpression(operator: Token, right: Expression) extends Expression
+
+case class InfixExpression(operator: Token, left: Expression, right: Expression) extends Expression
+
+case class FunctionLiteral(parameters: List[Identifier], body: BlockStatement) extends Expression
 
 case class CallExpression(function: Expression, arguments: List[Expression]) extends Expression
