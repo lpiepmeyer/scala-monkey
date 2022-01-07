2b4b6b5ff5a729dc34b01396542e9e5cad446dbb
lothar <lotharp@ixteam.de>
2022-01-07 10:11:10 +0100

    added REPL

--- src/main/scala/de/hfu/repl/Main.scala
+++ src/main/scala/de/hfu/repl/Main.scala
@@ -5,16 +5,30 @@ import de.hfu.lexer.TokenIterator
 import de.hfu.parser.Parser
 
 import java.io.FileReader
+import scala.io.StdIn.readLine
 
 object Main {
-  private def execute(filename:String): Unit = {
-    val reader=new FileReader(filename)
-    val actual = Evaluator(new Parser(TokenIterator(reader)).parseProgram(),new Context)
+  def main(args: Array[String]): Unit = {
+    if (args.length == 1) {
+      val filename = args(0)
+      execute(filename)
+    } else {
+      repl()
+    }
+  }
+
+  private def execute(filename: String): Unit = {
+    val reader = new FileReader(filename)
+    val actual = Evaluator(new Parser(TokenIterator(reader)).parseProgram(), new Context)
     println(actual)
   }
 
-  def main(args: Array[String]): Unit = {
-    val filename=args(0)
-    execute(filename)
+  def repl(): Unit = {
+    val context = new Context()
+    while (true) {
+      val input = readLine(">> ")
+      val actual = Evaluator(new Parser(TokenIterator(input)).parseProgram(), context)
+      println(actual)
+    }
   }
 }
